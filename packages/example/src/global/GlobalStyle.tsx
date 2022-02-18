13c3de68669b859da1a33b432cc8c80082831e89
= <=>
2022-02-18 14:32:25 -0300

    rename font

--- packages/example/src/global/GlobalStyle.tsx
+++ packages/example/src/global/GlobalStyle.tsx
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
-import HelveticaNeue from '../assets/fonts/SFUIRegular.woff2'
-import HelveticaNeueBold from '../assets/fonts/SFUIBlack.woff2'
-import HelveticaNeueMedium from '../assets/fonts/HelveticaNeueMedium.woff2'
+
 import { Colors, Fonts } from './styles'
+import Mac from '../assets/fonts/SFUIRegular.woff2'
+import MacBold from '../assets/fonts/HelveticaNeueMedium.woff2'
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -13,29 +13,27 @@ export const GlobalStyle = createGlobalStyle`
       display: none;
     }
   }
-
   @font-face {
     font-family: "HelveticaNeue";
-    src: url(${HelveticaNeue}) format('woff2');
+    src: url(${Mac}) format('woff2');
     font-weight: 400;
     font-style: normal;
     font-display: swap;
   }
   @font-face {
     font-family: "HelveticaNeue";
-    src: url(${HelveticaNeueMedium}) format('woff2');
+    src: url(${Mac}) format('woff2');
     font-weight: 500;
     font-style: normal;
     font-display: swap;
   }
   @font-face {
     font-family: "HelveticaNeue";
-    src: url(${HelveticaNeueBold}) format('woff2');
-    font-weight: 300;
+    src: url(${MacBold}) format('woff2');
+    font-weight: 700;
     font-style: normal;
     font-display: swap;
   }
-
   body, html {
     margin: 0;
     width: 100%;
@@ -56,33 +54,27 @@ export const GlobalStyle = createGlobalStyle`
     outline: none;
     cursor: pointer;
   }
-
   ul {
     list-style: none;
     margin: 0;
     padding: 0;
   }
-
   a {
     color: ${Colors.Black[900]};
     text-decoration: none;
-
     &:visited {
       color: ${Colors.Black[900]};
     }
   }
-
   h1, h2, h3, h4, h5, h6 {
     margin: 0;
     line-height: 150%;
     font-weight: 600;
     font-family: ${Fonts.Helvetica};
   }
-
   h1 {
     font-size: 32px;
   }
-
   h2 {
     font-size: 24px;
   }
@@ -90,11 +82,9 @@ export const GlobalStyle = createGlobalStyle`
   h3 {
     font-size: 14px;
   }
-
   h4 {
     font-size: 12px;
   }
-
   p {
     margin: 0;
   }
